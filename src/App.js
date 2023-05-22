bc87854c3008eb836d1393a6a65a9cbf7ddd885c
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2023-05-21 22:20:26 -0300

    feat: header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+import settings from './images/settings-white.png';
+
 function App() {
+  const [modalOpen, setModalOpen] = useState(false)
+
+  const openCloseModal = () => {
+    setModalOpen(!modalOpen);
+  }
+
   return (
-    <div className="App">
-      <h1>Pomodoro</h1>
+    <div className="app container-limit">
+      <header className="container-limit">
+        <button type="button" id="settings" onClick={openCloseModal}>
+          <img src={settings} alt="Imagem de um ícone que simboliza as configurações" />
+        </button>
+      </header>
+
     </div>
   );
 }
