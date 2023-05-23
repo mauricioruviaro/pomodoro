f32a69a93a00e2b0d7107da0dd356ce5266af0e5
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2023-05-22 21:35:31 -0300

    feat: container dos botoes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import { useState } from 'react';
+import Button from './components/Button.jsx';
 import settings from './images/settings-white.png';
 
 function App() {
-  const [modalOpen, setModalOpen] = useState(false)
+  const [modalOpen, setModalOpen] = useState(false);
+  const [option, setOption] = useState('focus');
+  const [defaultColor, setDefaultColor] = useState('red');
 
   const openCloseModal = () => {
     setModalOpen(!modalOpen);
   }
 
+  const changeOption = (opt) => {
+    if (opt === option) {
+      return false;
+    }
+    setOption(opt);
+  }
+
   return (
-    <div className="app container-limit">
+    <div className={`app ${defaultColor}`}>
       <header className="container-limit">
         <button type="button" id="settings" onClick={openCloseModal}>
           <img src={settings} alt="Imagem de um ícone que simboliza as configurações" />
         </button>
       </header>
+
+      <div className="buttons-container">
+        <Button
+          text="Foco"
+          classes={`${option === 'focus' && 'selected'}`}
+          onClick={() => changeOption('focus')}
+        />
+        <Button
+          text="Intervalo curto"
+          classes={`${option === 'short' && 'selected'}`}
+          onClick={() => changeOption('short')}
+        />
+        <Button
+          text="Intervalo longo"
+          classes={`${option === 'long' && 'selected'}`}
+          onClick={() => changeOption('long')}
+        />
+      </div>
 
     </div>
   );
